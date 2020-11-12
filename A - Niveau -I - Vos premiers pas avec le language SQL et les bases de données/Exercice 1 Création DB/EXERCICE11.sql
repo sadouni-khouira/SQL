6ceddfe4cc03bfaf0998bbd4cd29286a2ec2209d
Famille <Famille@Macbook-Pro.local>
2020-11-12 15:50:01 +0100

    SECOND_MODIFICATION

--- A - Niveau -I - Vos premiers pas avec le language SQL et les bases de données/Exercice 1 Création DB/EXERCICE11.sql
+++ A - Niveau -I - Vos premiers pas avec le language SQL et les bases de données/Exercice 1 Création DB/EXERCICE11.sql
@@ -10,7 +10,7 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
+/* REQUETTES*/
 -- Listage de la structure de la table bddexercice1. fournisseur
 CREATE TABLE IF NOT EXISTS `fournisseur` (
   `num_f` int(8) NOT NULL,
